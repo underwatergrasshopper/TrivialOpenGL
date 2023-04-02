18428c0a199c94c4a06fb8bcf4e7982ad7573d8a
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-04-02 14:24:31 +0200

    Updated TrivialOpenGL_Key.h.

--- include/TrivialOpenGL_Key.h
+++ include/TrivialOpenGL_Key.h
@@ -13,29 +13,34 @@ namespace TrivialOpenGL {
     enum KeyId {
         KEY_ID_UNKNOWN,
 
-        KEY_ID_BREAK,            
-        KEY_ID_BACKSPACE,
-        KEY_ID_TAB,
+        KEY_ID_ESCAPE,
         KEY_ID_ENTER,  
+        KEY_ID_CAPS_LOCK,   
+        KEY_ID_TAB,    
+        KEY_ID_BACKSPACE,
+        KEY_ID_SPACE,
+
         KEY_ID_SHIFT,
         KEY_ID_CONTROL,
         KEY_ID_ALT, 
-        KEY_ID_PAUSE,
-        KEY_ID_CAPS_LOCK,       
-        KEY_ID_ESCAPE,
-        KEY_ID_SPACE,
+
         KEY_ID_PAGE_UP,               
         KEY_ID_PAGE_DOWN,       
         KEY_ID_END, 
         KEY_ID_HOME,  
+
         KEY_ID_ARROW_LEFT,  
         KEY_ID_ARROW_UP, 
         KEY_ID_ARROW_RIGHT,
         KEY_ID_ARROW_DOWN,
-        KEY_ID_PRINT,
-        KEY_ID_PRINT_SCREEN,
+
         KEY_ID_INSERT,
         KEY_ID_DELETE,
+        KEY_ID_BREAK,        
+        KEY_ID_PAUSE,
+        KEY_ID_PRINT,
+        KEY_ID_PRINT_SCREEN,
+
         KEY_ID_NUMPAD_0,
         KEY_ID_NUMPAD_1,
         KEY_ID_NUMPAD_2,
@@ -52,6 +57,44 @@ namespace TrivialOpenGL {
         KEY_ID_NUMPAD_SUBTRACT,
         KEY_ID_NUMPAD_DECIMAL,
         KEY_ID_NUMPAD_DIVIDE,
+
+        KEY_ID_0,
+        KEY_ID_1,
+        KEY_ID_2,
+        KEY_ID_3,
+        KEY_ID_4,
+        KEY_ID_5,
+        KEY_ID_6,
+        KEY_ID_7,
+        KEY_ID_8,
+        KEY_ID_9,
+        KEY_ID_A,
+        KEY_ID_B,
+        KEY_ID_C,
+        KEY_ID_D,
+        KEY_ID_E,
+        KEY_ID_F,
+        KEY_ID_G,
+        KEY_ID_H,
+        KEY_ID_I,
+        KEY_ID_J,
+        KEY_ID_K,
+        KEY_ID_L,
+        KEY_ID_M,
+        KEY_ID_N,
+        KEY_ID_O,
+        KEY_ID_P,
+        KEY_ID_Q,
+        KEY_ID_R,
+        KEY_ID_S,
+        KEY_ID_T,
+        KEY_ID_U,
+        KEY_ID_V,
+        KEY_ID_W,
+        KEY_ID_X,
+        KEY_ID_Y,
+        KEY_ID_Z,
+
         KEY_ID_F1,
         KEY_ID_F2,
         KEY_ID_F3,
@@ -76,8 +119,10 @@ namespace TrivialOpenGL {
         KEY_ID_F22,
         KEY_ID_F23,
         KEY_ID_F24,
+
         KEY_ID_NUMLOCK,
         KEY_ID_SCROLL_LOCK,
+
         KEY_ID_LEFT_SHIFT, 
         KEY_ID_RIGHT_SHIFT,
         KEY_ID_LEFT_CONTROL, 
@@ -91,77 +136,113 @@ namespace TrivialOpenGL {
 
     inline KeyId VK_CodeToKeyId(int vk_code) {
         switch (vk_code) {
-        case VK_CANCEL:           return KEY_ID_BREAK;                  
-        case VK_BACK:             return KEY_ID_BACKSPACE;              
-        case VK_TAB:              return KEY_ID_TAB;                    
-        case VK_RETURN:           return KEY_ID_ENTER;                  
-        case VK_SHIFT:            return KEY_ID_SHIFT;                  
-        case VK_CONTROL:          return KEY_ID_CONTROL;                
-        case VK_MENU:             return KEY_ID_ALT;                    
-        case VK_PAUSE:            return KEY_ID_PAUSE;                  
-        case VK_CAPITAL:          return KEY_ID_CAPS_LOCK;              
-        case VK_ESCAPE:           return KEY_ID_ESCAPE;                 
-        case VK_SPACE:            return KEY_ID_SPACE;                  
-        case VK_PRIOR:            return KEY_ID_PAGE_UP;                
-        case VK_NEXT:             return KEY_ID_PAGE_DOWN;              
-        case VK_END:              return KEY_ID_END;                    
-        case VK_HOME:             return KEY_ID_HOME;                   
-        case VK_LEFT:             return KEY_ID_ARROW_LEFT;             
-        case VK_UP:               return KEY_ID_ARROW_UP;               
-        case VK_RIGHT:            return KEY_ID_ARROW_RIGHT;            
-        case VK_DOWN:             return KEY_ID_ARROW_DOWN;             
-        case VK_PRINT:            return KEY_ID_PRINT;                  
-        case VK_SNAPSHOT:         return KEY_ID_PRINT_SCREEN;           
-        case VK_INSERT:           return KEY_ID_INSERT;                 
-        case VK_DELETE:           return KEY_ID_DELETE;                 
-        case VK_NUMPAD0:          return KEY_ID_NUMPAD_0;               
-        case VK_NUMPAD1:          return KEY_ID_NUMPAD_1;               
-        case VK_NUMPAD2:          return KEY_ID_NUMPAD_2;               
-        case VK_NUMPAD3:          return KEY_ID_NUMPAD_3;               
-        case VK_NUMPAD4:          return KEY_ID_NUMPAD_4;               
-        case VK_NUMPAD5:          return KEY_ID_NUMPAD_5;               
-        case VK_NUMPAD6:          return KEY_ID_NUMPAD_6;               
-        case VK_NUMPAD7:          return KEY_ID_NUMPAD_7;               
-        case VK_NUMPAD8:          return KEY_ID_NUMPAD_8;               
-        case VK_NUMPAD9:          return KEY_ID_NUMPAD_9;               
-        case VK_MULTIPLY:         return KEY_ID_NUMPAD_MULTIPLY;        
-        case VK_ADD:              return KEY_ID_NUMPAD_ADD;             
-        case VK_SEPARATOR:        return KEY_ID_NUMPAD_SEPARATOR;       
-        case VK_SUBTRACT:         return KEY_ID_NUMPAD_SUBTRACT;        
-        case VK_DECIMAL:          return KEY_ID_NUMPAD_DECIMAL;         
-        case VK_DIVIDE:           return KEY_ID_NUMPAD_DIVIDE;          
-        case VK_F1:               return KEY_ID_F1;                     
-        case VK_F2:               return KEY_ID_F2;                     
-        case VK_F3:               return KEY_ID_F3;                     
-        case VK_F4:               return KEY_ID_F4;                     
-        case VK_F5:               return KEY_ID_F5;                     
-        case VK_F6:               return KEY_ID_F6;                     
-        case VK_F7:               return KEY_ID_F7;                     
-        case VK_F8:               return KEY_ID_F8;                     
-        case VK_F9:               return KEY_ID_F9;                     
-        case VK_F10:              return KEY_ID_F10;                    
-        case VK_F11:              return KEY_ID_F11;                    
-        case VK_F12:              return KEY_ID_F12;                    
-        case VK_F13:              return KEY_ID_F13;                    
-        case VK_F14:              return KEY_ID_F14;                    
-        case VK_F15:              return KEY_ID_F15;                    
-        case VK_F16:              return KEY_ID_F16;                    
-        case VK_F17:              return KEY_ID_F17;                    
-        case VK_F18:              return KEY_ID_F18;                    
-        case VK_F19:              return KEY_ID_F19;                    
-        case VK_F20:              return KEY_ID_F20;                    
-        case VK_F21:              return KEY_ID_F21;                    
-        case VK_F22:              return KEY_ID_F22;                    
-        case VK_F23:              return KEY_ID_F23;                    
-        case VK_F24:              return KEY_ID_F24;                    
-        case VK_NUMLOCK:          return KEY_ID_NUMLOCK;                
-        case VK_SCROLL:           return KEY_ID_SCROLL_LOCK;            
-        case VK_LSHIFT:           return KEY_ID_LEFT_SHIFT;             
-        case VK_RSHIFT:           return KEY_ID_RIGHT_SHIFT;            
-        case VK_LCONTROL:         return KEY_ID_LEFT_CONTROL;           
-        case VK_RCONTROL:         return KEY_ID_RIGHT_CONTROL;          
-        case VK_LMENU:            return KEY_ID_LEFT_ALT;               
-        case VK_RMENU:            return KEY_ID_RIGHT_ALT;    
+        case VK_CANCEL:         return KEY_ID_BREAK;                  
+        case VK_BACK:           return KEY_ID_BACKSPACE;              
+        case VK_TAB:            return KEY_ID_TAB;                    
+        case VK_RETURN:         return KEY_ID_ENTER;                  
+        case VK_SHIFT:          return KEY_ID_SHIFT;                  
+        case VK_CONTROL:        return KEY_ID_CONTROL;                
+        case VK_MENU:           return KEY_ID_ALT;                    
+        case VK_PAUSE:          return KEY_ID_PAUSE;                  
+        case VK_CAPITAL:        return KEY_ID_CAPS_LOCK;              
+        case VK_ESCAPE:         return KEY_ID_ESCAPE;                 
+        case VK_SPACE:          return KEY_ID_SPACE;                  
+        case VK_PRIOR:          return KEY_ID_PAGE_UP;                
+        case VK_NEXT:           return KEY_ID_PAGE_DOWN;              
+        case VK_END:            return KEY_ID_END;                    
+        case VK_HOME:           return KEY_ID_HOME;                   
+        case VK_LEFT:           return KEY_ID_ARROW_LEFT;             
+        case VK_UP:             return KEY_ID_ARROW_UP;               
+        case VK_RIGHT:          return KEY_ID_ARROW_RIGHT;            
+        case VK_DOWN:           return KEY_ID_ARROW_DOWN;             
+        case VK_PRINT:          return KEY_ID_PRINT;                  
+        case VK_SNAPSHOT:       return KEY_ID_PRINT_SCREEN;           
+        case VK_INSERT:         return KEY_ID_INSERT;                 
+        case VK_DELETE:         return KEY_ID_DELETE;                 
+        case VK_NUMPAD0:        return KEY_ID_NUMPAD_0;               
+        case VK_NUMPAD1:        return KEY_ID_NUMPAD_1;               
+        case VK_NUMPAD2:        return KEY_ID_NUMPAD_2;               
+        case VK_NUMPAD3:        return KEY_ID_NUMPAD_3;               
+        case VK_NUMPAD4:        return KEY_ID_NUMPAD_4;               
+        case VK_NUMPAD5:        return KEY_ID_NUMPAD_5;               
+        case VK_NUMPAD6:        return KEY_ID_NUMPAD_6;               
+        case VK_NUMPAD7:        return KEY_ID_NUMPAD_7;               
+        case VK_NUMPAD8:        return KEY_ID_NUMPAD_8;               
+        case VK_NUMPAD9:        return KEY_ID_NUMPAD_9;               
+        case VK_MULTIPLY:       return KEY_ID_NUMPAD_MULTIPLY;        
+        case VK_ADD:            return KEY_ID_NUMPAD_ADD;             
+        case VK_SEPARATOR:      return KEY_ID_NUMPAD_SEPARATOR;       
+        case VK_SUBTRACT:       return KEY_ID_NUMPAD_SUBTRACT;        
+        case VK_DECIMAL:        return KEY_ID_NUMPAD_DECIMAL;         
+        case VK_DIVIDE:         return KEY_ID_NUMPAD_DIVIDE;          
+        case VK_F1:             return KEY_ID_F1;                     
+        case VK_F2:             return KEY_ID_F2;                     
+        case VK_F3:             return KEY_ID_F3;                     
+        case VK_F4:             return KEY_ID_F4;                     
+        case VK_F5:             return KEY_ID_F5;                     
+        case VK_F6:             return KEY_ID_F6;                     
+        case VK_F7:             return KEY_ID_F7;                     
+        case VK_F8:             return KEY_ID_F8;                     
+        case VK_F9:             return KEY_ID_F9;                     
+        case VK_F10:            return KEY_ID_F10;                    
+        case VK_F11:            return KEY_ID_F11;                    
+        case VK_F12:            return KEY_ID_F12;                    
+        case VK_F13:            return KEY_ID_F13;                    
+        case VK_F14:            return KEY_ID_F14;                    
+        case VK_F15:            return KEY_ID_F15;                    
+        case VK_F16:            return KEY_ID_F16;                    
+        case VK_F17:            return KEY_ID_F17;                    
+        case VK_F18:            return KEY_ID_F18;                    
+        case VK_F19:            return KEY_ID_F19;                    
+        case VK_F20:            return KEY_ID_F20;                    
+        case VK_F21:            return KEY_ID_F21;                    
+        case VK_F22:            return KEY_ID_F22;                    
+        case VK_F23:            return KEY_ID_F23;                    
+        case VK_F24:            return KEY_ID_F24;   
+        case '0':               return KEY_ID_0;	
+        case '1':               return KEY_ID_1;	
+        case '2':               return KEY_ID_2;	
+        case '3':               return KEY_ID_3;	
+        case '4':               return KEY_ID_4;	
+        case '5':               return KEY_ID_5;	
+        case '6':               return KEY_ID_6;	
+        case '7':               return KEY_ID_7;	
+        case '8':               return KEY_ID_8;	
+        case '9':               return KEY_ID_9;	
+        case 'A':               return KEY_ID_A;	
+        case 'B':               return KEY_ID_B;	
+        case 'C':               return KEY_ID_C;	
+        case 'D':               return KEY_ID_D;	
+        case 'E':               return KEY_ID_E;	
+        case 'F':               return KEY_ID_F;	
+        case 'G':               return KEY_ID_G;	
+        case 'H':               return KEY_ID_H;	
+        case 'I':               return KEY_ID_I;	
+        case 'J':               return KEY_ID_J;	
+        case 'K':               return KEY_ID_K;	
+        case 'L':               return KEY_ID_L;	
+        case 'M':               return KEY_ID_M;	
+        case 'N':               return KEY_ID_N;	
+        case 'O':               return KEY_ID_O;	
+        case 'P':               return KEY_ID_P;	
+        case 'Q':               return KEY_ID_Q;	
+        case 'R':               return KEY_ID_R;	
+        case 'S':               return KEY_ID_S;	
+        case 'T':               return KEY_ID_T;	
+        case 'U':               return KEY_ID_U;	
+        case 'V':               return KEY_ID_V;	
+        case 'W':               return KEY_ID_W;	
+        case 'X':               return KEY_ID_X;	
+        case 'Y':               return KEY_ID_Y;	
+        case 'Z':               return KEY_ID_Z;
+        case VK_NUMLOCK:        return KEY_ID_NUMLOCK;                
+        case VK_SCROLL:         return KEY_ID_SCROLL_LOCK;            
+        case VK_LSHIFT:         return KEY_ID_LEFT_SHIFT;             
+        case VK_RSHIFT:         return KEY_ID_RIGHT_SHIFT;            
+        case VK_LCONTROL:       return KEY_ID_LEFT_CONTROL;           
+        case VK_RCONTROL:       return KEY_ID_RIGHT_CONTROL;          
+        case VK_LMENU:          return KEY_ID_LEFT_ALT;               
+        case VK_RMENU:          return KEY_ID_RIGHT_ALT;    
         }
         return KEY_ID_UNKNOWN;
     }
@@ -246,6 +327,7 @@ namespace TrivialOpenGL {
         TOGL_INNER_CASE_STR(VK_LMENU);         
         TOGL_INNER_CASE_STR(VK_RMENU);   
         }
+        if ((vk_code >= '0' && vk_code <= '9') || (vk_code >= 'A' && vk_code <= 'Z')) return std::string(1, (char)vk_code); 
         return std::string() + "(" + std::to_string(vk_code) + ")";
     }
 #undef TOGL_INNER_CASE_STR
